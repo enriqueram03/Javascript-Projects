135913a7607a0b5a53d57f50c1f14dc45de7e253
enriqueram03 <ramirezramos2003@gmail.com>
2021-04-26 09:18:55 -0700

    function

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,1 +1,5 @@
-document.write(4+4);
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
