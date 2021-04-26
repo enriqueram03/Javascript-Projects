02fbbb29e0552f1d80a6e81f90921582f88871f3
enriqueram03 <ramirezramos2003@gmail.com>
2021-04-26 09:23:21 -0700

    added notes to the function

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,5 +1,5 @@
-function My_First_Function() {
-    var str = "This text is green!";
-    var result = str.fontcolor("green");
-    document.getElementById("Green_Text").innerHTML = result;
+function My_First_Function() {  //Defining a function and naming it
+    var str = "This text is green!";  //Defining a variable and giving it a string function
+    var result = str.fontcolor("green");  //Using the fontcolor method on str variable
+    document.getElementById("Green_Text").innerHTML = result; //Putting the value of result into HTML element with "Green_Text" id
 }
