9dfea733e04c0a2984a04f1346b58437a1824b31
enriqueram03 <ramirezramos2003@gmail.com>
2021-04-27 12:26:23 -0700

    got functions and put infinity

--- Project5_comparisons_type_coercion/JS/comparison.js
+++ Project5_comparisons_type_coercion/JS/comparison.js
@@ -3,3 +3,17 @@ document.write(typeof "Word");
 document.write(typeof 3);
 
 document.write("10" + 5);
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+function my_function() {
+document.getElementById("test").innerHTML = isNaN('This is a string');
+}
+
+function my_Ffunction() {
+    document.getElementById("Ttest").innerHTML = isNaN('007');
+}
+
+document.write(2E310);
