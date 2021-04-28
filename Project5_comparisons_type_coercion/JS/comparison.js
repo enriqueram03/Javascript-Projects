a610d720ce31e2ba47a70e343e37f0cb99d5634d
enriqueram03 <ramirezramos2003@gmail.com>
2021-04-28 10:01:03 -0700

    yes

--- Project5_comparisons_type_coercion/JS/comparison.js
+++ Project5_comparisons_type_coercion/JS/comparison.js
@@ -1,29 +1,41 @@
-document.write(typeof "Word");
+document.write(typeof "Word"); //String
 
-document.write(typeof 3);
+document.write(typeof 3); //Number
 
-document.write("10" + 5);
+document.write("10" + 5); //105
 
 function my_Function() {
-    document.getElementById("Test").innerHTML = 0/0;
+    document.getElementById("Test").innerHTML = 0/0;  //NaN
 }
 
 function my_function() {
-document.getElementById("test").innerHTML = isNaN('This is a string');
+document.getElementById("test").innerHTML = isNaN('This is a string'); //true
 }
 
 function my_Ffunction() {
-    document.getElementById("Ttest").innerHTML = isNaN('007');
+    document.getElementById("Ttest").innerHTML = isNaN('007'); //false
 }
 
-document.write(2E310);
+document.write(2E310); //Infinity
 
-document.write(-3E310);
+document.write(-3E310); //Negative Infinity
 
-document.write(10 > 2);
+document.write(10 > 2); //Boolean Logic true
 
-document.write(10 < 2);
+document.write(10 < 2); //Boolean logic false
 
-console.log(2 + 4);
+console.log(2 + 4); //Makes it appear in the console
 
-console.log(2 + 4 > 8);
+console.log(2 + 4 > 8); //Using boolean logic in the console
+
+document.write(10 == 10); // true using the == symbol to check for equality first item compared to the second item
+
+document.write(3 == 11); // false since the 3 and 11 arent equal
+
+X = 10;
+Y = 10;
+document.write(X === Y); // the compuer checks if the 2 pieces of data are equal in both Data and Type this is true
+
+X = 82;
+Y = "82";
+document.write(X === Y);
