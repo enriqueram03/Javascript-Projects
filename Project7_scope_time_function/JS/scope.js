c5ab73a780df235a88f4afbc8afaa442b66d7d2a
enriqueram03 <ramirezramos2003@gmail.com>
2021-04-28 15:27:41 -0700

    stuff

--- Project7_scope_time_function/JS/scope.js
+++ Project7_scope_time_function/JS/scope.js
@@ -37,3 +37,14 @@ function get_Date(){ // The Date().getHours() method returns the hour in the spe
         document.getElementById("Greeting").innerHTML = "How are you today?";
     }
 }
+
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
