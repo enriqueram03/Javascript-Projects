de715b9088de2259a2a4d1c1a19390f0b24b7504
enriqueram03 <ramirezramos2003@gmail.com>
2021-04-30 10:59:48 -0700

    added uppercase and search method

--- Project8_string_methods/JS/string.js
+++ Project8_string_methods/JS/string.js
@@ -1,8 +1,26 @@
-function full_Sentence() {
+function full_Sentence() { // you can make all the parts into a full sentence
     var part_1 = "I have ";
     var part_2 = "made this ";
     var part_3 = "into a complete ";
     var part_4 = "sentence. ";
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+function slice_Method() { // chosses specific text to show
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function Bumpin() { // makes everything upper case
+    var str = "Outer Space";
+    var res = str.toUpperCase();
+    document.getElementById("saitama").innerHTML = res;
+}
+
+function Talking() {
+    var str = "Visit Portland";
+    var e = str.search("Portland");
+    document.getElementById("Adults").innerHTML = e;
 }
