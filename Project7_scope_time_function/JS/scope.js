581c6241bd1249524181695d9c3ac6761b9763ec
enriqueram03 <ramirezramos2003@gmail.com>
2021-04-30 10:28:38 -0700

    used concat method

--- Project7_scope_time_function/JS/scope.js
+++ Project7_scope_time_function/JS/scope.js
@@ -48,3 +48,18 @@ function Age_Function() {
     }
     document.getElementById("How_old_are_you?").innerHTML = Vote;
 }
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) { //else if statement follows the if statement and is executed if the "if" statement is found to be false
+        Reply ="It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
