2628a95f8b9b8bd167647da87443af76dd6373fc
enriqueram03 <ramirezramos2003@gmail.com>
2021-05-02 11:46:15 -0700

    stuff

--- Project10_loops_arrays/JS/loops.js
+++ Project10_loops_arrays/JS/loops.js
@@ -13,3 +13,23 @@ function Tales() {
     var n = str.length;
     document.getElementById("loner").innerHTML = n;
 }
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+    Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function cat_pics() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " +
+        Cat_Picture[2] + ".";
+}
