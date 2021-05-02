8be266fee65b9f02030a6dd54c0d0c1b62b2ad01
enriqueram03 <ramirezramos2003@gmail.com>
2021-05-02 14:34:37 -0700

    notes

--- Project10_loops_arrays/JS/loops.js
+++ Project10_loops_arrays/JS/loops.js
@@ -71,3 +71,14 @@ function returner() {
     return Math.PI;
 }
 document.getElementById("demo").innerHTML = returner();
+
+let car = {
+    make: "Dodge",
+    model: "Viper",
+    year: "2021", 
+    color: "red", 
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
