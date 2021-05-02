9bd002e41d9879d82176a30008bb6eda62cff04f
enriqueram03 <ramirezramos2003@gmail.com>
2021-05-02 11:51:40 -0700

    const keyword

--- Project10_loops_arrays/JS/loops.js
+++ Project10_loops_arrays/JS/loops.js
@@ -33,3 +33,11 @@ function cat_pics() {
     document.getElementById("Cat").innerHTML = "In this picture, the cat is " +
         Cat_Picture[2] + ".";
 }
+
+function constant_function() {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " +
+        Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
