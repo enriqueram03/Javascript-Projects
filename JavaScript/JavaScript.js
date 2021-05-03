0d0a308c41ef5b44c0f6ea4b77cf797c1838943c
enriqueram03 <ramirezramos2003@gmail.com>
2021-05-03 11:11:30 -0700

    Update JavaScript.js

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,1 +1,28 @@
-function Song_Function
+function Song_Function() {
+    var Song_Output;
+    var Songs = document.getElementById("Song_Input").value;
+    var Song_String = " is a great song!";
+    switch(Songs) {
+        case "Purge":
+            Song_Output = "Purge" + Song_String;
+        break;
+        case "Hot Girl Bummer":
+            Song_Output = "Hot Girl Bummer" + Song_String;
+        break;
+        case "Rigamortus":
+            Song_Output = "Rigamortus" + Song_String;
+        break;
+        case "Yeah Right":
+            Song_Output = "Yeah Right" + Song_String;
+        break;
+        case "Myself":
+            Song_Output = "Myself" + Song_String;
+        break;
+        case "The Adults Are Talking":
+            Song_Output = "The Adults Are Talking" + Song_String;
+        break;
+        default:
+        Song_Output = "Please enter a song exactly as written on the list above.";
+    }
+    document.getElementById("Output").innerHTML = Song_Output;
+}
